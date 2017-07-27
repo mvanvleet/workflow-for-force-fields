f850db9b32c3b253eb855054995f5311ea9a71f3
Mary Van Vleet <mvanvleet@chem.wisc.edu>
2017-07-27 15:51:13 -0500

    Make CCSD(T)-f12 input file generator.

--- scripts/make_ccsdt_ifiles.py
+++ scripts/make_ccsdt_ifiles.py
@@ -16,7 +16,7 @@ import subprocess
 # mvanvleet specific modules
 from chemistry import io, geometry, elementdata
 #from generate_geometries.generate_grid_points import GenerateGridPoints
-from xyz.add_midbond import add_midbond
+from add_midbond import add_midbond
 
 ###########################################################################
 ####################### Global Variables ##################################
@@ -35,8 +35,7 @@ scriptsdir = maindir + '/scripts/'
 templatesdir = maindir + '/templates/'
 inputdir = maindir + '/input/'
 geometriesdir = maindir + '/geometries/'
-saptdir = maindir + '/sapt/input/'
-ipdir = maindir + '/sapt/monomer_calcs/'
+ccsdtdir = maindir + '/ccsdt/input/'
 
 dimer_info_file = 'dimer_info.dat'
 
@@ -104,29 +103,6 @@ class CreateSAPTInputFile():
 
 
 ###########################################################################
-    def get_ip_data(self):
-        '''
-        '''
-        print 'Fetching IP data.'
-        ofile1 = ipdir + self.mona + '_ip.out'
-        ip1 = subprocess.check_output([scriptsdir + '/print_homo_ip.sh',ofile1])
-
-        ofile2 = ipdir + self.monb + '_ip.out'
-        ip2 = subprocess.check_output([scriptsdir + '/print_homo_ip.sh',ofile2])
-
-        with open(templatesdir + 'ips.dat','w') as f:
-            f.write(self.mona + '\t')
-            f.write(ip1)
-            f.write('\n')
-            f.write(self.monb + '\t')
-            f.write(ip2)
-            f.write('\n')
-
-        return
-###########################################################################
-
-
-###########################################################################
     def add_midbonds(self,input_dir):
         '''
         '''
@@ -187,9 +163,9 @@ class CreateSAPTInputFile():
 
 
 ###########################################################################
-    def fill_sapt_template(self,dir,\
-            template_file='pbe0_template.com',\
-            ip_file='templates/ips.dat'):
+    def fill_ccsdt_template(self,dir,\
+            template_file='ccsdt_template.com',\
+            ):
         '''
         '''
 
@@ -204,14 +180,9 @@ class CreateSAPTInputFile():
         fill_q_dimer = 'FILL_Q_DIMER'
         fill_q_mona = 'FILL_Q_MONA'
         fill_q_monb = 'FILL_Q_MONB'
-        fill_ip_mona = 'FILL_IP_MONA'
-        fill_ip_monb = 'FILL_IP_MONB'
-        fill_homo_mona = 'FILL_HOMO_MONA'
-        fill_homo_monb = 'FILL_HOMO_MONB'
         fill_items = [ fill_mona, fill_monb, fill_i_mona, fill_i_monb,
                         fill_q_dimer, fill_q_mona, fill_q_monb,
-                        fill_ip_mona, fill_ip_monb, 
-                        fill_homo_mona, fill_homo_monb ]
+                     ]
 
         mona = self.mona
         monb = self.monb
@@ -222,60 +193,42 @@ class CreateSAPTInputFile():
         q_monb = self.q_monb
         q_dimer = q_mona + q_monb
 
-        if self.ignorecase:
-            info_mona = subprocess.check_output(['grep','-iw',self.mona,ip_file])
-            info_monb = subprocess.check_output(['grep','-iw',self.monb,ip_file])
-        else:
-            info_mona = subprocess.check_output(['grep','-w',self.mona,ip_file])
-            info_monb = subprocess.check_output(['grep','-w',self.monb,ip_file])
-
-        info_mona = info_mona.split()
-        info_monb = info_monb.split()
-
-        ip_mona = float(info_mona[2]) #column 2 for dft ip's, column 3 for exp ip's
-        ip_monb = float(info_monb[2])
-        homo_mona = float(info_mona[1])
-        homo_monb = float(info_monb[1])
-
         items = [ mona, monb, i_mona, i_monb,
                   q_dimer, q_mona, q_monb,
-                  ip_mona, ip_monb, 
-                  homo_mona, homo_monb ]
+                ]
 
         subprocess.call(['cp',template_filepath,filepath])
         for [fill,item] in zip(fill_items,items):
             subprocess.call(['sed','-i',"s/"+fill+'/'+str(item)+'/g',filepath])
 
         return
-
-
 ###########################################################################
 
 
 ###########################################################################
-    def create_sapt_input_files(self,saptdir,geometriesdir,\
-            sapt_template_file='pbe0_template.com'):
+    def create_ccsdt_input_files(self,ccsdtdir,geometriesdir,\
+            ccsdt_template_file='ccsdt_template.com'):
             
         '''
         '''
 
-        print 'Creating SAPT input file.'
+        print 'Creating CCSD(T) input file.'
         homedir = os.getcwd()
-        input_filepath = saptdir+'/'+self.mona+'_'+self.monb+'_'+ sapt_template_file
+        input_filepath = ccsdtdir+'/'+self.mona+'_'+self.monb+'_'+ ccsdt_template_file
         
         # Copy xyz files to input directory (using xargs in case argument list
         # is long)
-        subprocess.call('echo '+ geometriesdir + '/*xyz  | xargs cp -t '+ saptdir, shell=True)
-        os.chdir(saptdir)
+        subprocess.call('echo '+ geometriesdir + '/*xyz  | xargs cp -t '+ ccsdtdir, shell=True)
+        os.chdir(ccsdtdir)
 
-        self.add_midbonds(saptdir)
+        self.add_midbonds(ccsdtdir)
 
         # Create input files corresponding to all .xyz configurations
         runscript = scriptsdir + 'create_molpro_files_from_xyz.py'
         xyz_prefix = self.mona + '_' + self.monb
-        sapt_prefix = self.mona + '_' + self.monb + '_' + sapt_template_file.rstrip('_template.com')
+        ccsdt_prefix = self.mona + '_' + self.monb + '_' + ccsdt_template_file.replace('_template.com','')
 
-        subprocess.call([runscript, sapt_prefix, xyz_prefix])
+        subprocess.call([runscript, ccsdt_prefix, xyz_prefix])
         os.chdir(homedir)
 
         return 
@@ -290,20 +243,19 @@ class CreateSAPTInputFile():
 ######################## Command Line Arguments ###########################
 
 # Make input directory
-subprocess.call(['mkdir','-p',saptdir])
+subprocess.call(['mkdir','-p',ccsdtdir])
 
 # Create SAPT template file
 c = CreateSAPTInputFile(dimer_info_file, inputdir, templatesdir)
-c.get_ip_data()
-c.fill_sapt_template(saptdir)
+c.fill_ccsdt_template(ccsdtdir)
 
 # Copy xyz files over to the input directory, add midbond functions as
 # necessary, and create input files for each .xyz configuration
-c.create_sapt_input_files(saptdir,geometriesdir)
+c.create_ccsdt_input_files(ccsdtdir,geometriesdir)
 
-# Copy basis set file over to sapt directory
+# Copy basis set file over to ccsdt directory
 basisfile=templatesdir + '/basis_sets/AVTZ.mbas'
-subprocess.call(['cp',basisfile,saptdir])
+subprocess.call(['cp',basisfile,ccsdtdir])
 
         
 
